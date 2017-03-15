c66c33500c9df054e264755e8db5af21f4e99a2d
Isaac Peifer <iwpeifer@gmail.com>
2017-03-15 20:16:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,30 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  organized_pigeons = {}
+  data.each do |category, stats|
+    stats.each do |stat, name_array|
+      name_array.each do |name|
+        organized_pigeons[name] = {}
+      end
+    end
+  end
+  organized_pigeons.each do |name, stat_hash|
+    stat_hash[:color] = []
+    stat_hash[:gender] = []
+    stat_hash[:lives] = []
+    stat_hash.each do |cat, stat_array|
+      data.each do |category, stats|
+        stats.each do |stat, name_array|
+          if name_array.include?(name)
+            stat_hash[category] << stat.to_s
+          end
+        end
+      end
+    end
+  end
+  organized_pigeons.each do |name, stat_hash|
+    stat_hash.each do |cat, stat_array|
+      stat_array.uniq!
+    end
+  end
+  organized_pigeons
 end
